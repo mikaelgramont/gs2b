8608a7c89e0b6591b4169fc73e8a34efc14c56b9
mikaelgramont <mgramont@gmail.com>
2014-04-13 14:20:35 -0700

    Finished initial animation system

--- gs2b.js
+++ gs2b.js
@@ -1,25 +1,46 @@
 (function(window, console) {
-  var canvas = document.getElementById('the-canvas'),
-      context = canvas.getContext('2d'),
-      button = document.getElementById('do-it'),
+  var buttonEl = document.getElementById('do-it'),
       animationCounter = 0,
+      frameCounter = 0,
+      totalAnimations = 0,
+      totalFrames = 0,
       spriteInfo = {
         'source': document.getElementById('spritesheet'),
         'width': 32,
         'height': 32,
         'numFrames': 8,
+        'fps': 2,
         'current': 0,
       },
       canvasInfo = {
         'width': 500,
         'height': 500,
       };
+  const FPS = 60;
+
+  var canvasEl = document.createElement('canvas');
+  canvasEl.width = canvasInfo.width;
+  canvasEl.height = spriteInfo.numFrames * canvasInfo.height;
+  canvasEl.id = 'the-canvas';
+  document.getElementById('fun-container').appendChild(canvasEl);
+  var context = canvasEl.getContext('2d');
 
   var animationTick = function() {
+    totalAnimations += 1;
     animationCounter += 1;
-    if (animationCounter >= spriteInfo.numFrames) {
+    if (animationCounter >= FPS) {
       animationCounter = 0;
     }
+
+    if (animationCounter % (FPS / spriteInfo.fps) == 0) {
+      frameCounter += 1;
+      totalFrames += 1;
+    }
+
+    if (frameCounter >= spriteInfo.numFrames) {
+      frameCounter = 0;
+    }
+    moveCanvas();
     requestAnimationFrame(animationTick);
   };
 
@@ -27,7 +48,7 @@
     var destinationPosition = getRandomPosition();
 
     for (var i = 0; i < spriteInfo.numFrames; i++) {
-      var offset = (animationCounter + i) % spriteInfo.numFrames;
+      var offset = (frameCounter + i) % spriteInfo.numFrames;
       context.drawImage(
         spriteInfo.source,            // source
         0,                            // source x
@@ -35,7 +56,7 @@
         spriteInfo.width,             // source width
         spriteInfo.height,            // source height
         destinationPosition.x,        // destination x
-        destinationPosition.y,        // destination y
+        destinationPosition.y + i * canvasInfo.height,        // destination y
         spriteInfo.width,             // destination width
         spriteInfo.height);           // destination height
     }
@@ -48,7 +69,10 @@
     return {'x':x, 'y':y};
   };
 
+  var moveCanvas = function() {
+    canvasEl.style.top = (-1 * frameCounter * canvasInfo.height) + 'px';
+  };
 
-  button.addEventListener('click', drawSprite);
+  buttonEl.addEventListener('click', drawSprite);
   requestAnimationFrame(animationTick);
 })(window, console);
